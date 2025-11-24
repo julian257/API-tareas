f9bf2dfcf5ee7357961eed79526764c5c5ca00e7
julian257 <julian.anguiano23b@udgvirtual.udg.mx>
2025-11-24 10:22:31 -0600

    cambio a contacto

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TareaController;
+use App\Http\Controllers\ContactoController;
 
-Route::apiResource('tareas', TareaController::class);
+Route::get("/contactos", [ContactoController::class, "index"]);
+Route::post("/contactos", [ContactoController::class, "store"]);
+Route::get("/contactos/{id}", [ContactoController::class, "show"]);
+Route::put("/contactos/{id}", [ContactoController::class, "update"]);
+Route::delete("/contactos/{id}", [ContactoController::class, "destroy"]);
